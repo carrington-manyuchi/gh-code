8dd67a5057ea5c0a74b4fee54c5553bb99e7d664
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-04 18:11:00 +0200

    Initial Commit

---
+++ gh-code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gh-code
+//
+//  Created by Manyuchi, Carrington C on 2024/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
